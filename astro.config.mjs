2bc457ffb9c8d7b5470a1d4bba09f80f41b5a66e
Danijela Čolić <dgaric1@gmail.com>
2025-09-25 11:53:24 +0200

    Refresh lockfile + use @astrojs/vercel/server

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel/server";
+import vercel from "@astrojs/vercel/server"; // VAŽNO
 
 export default defineConfig({
   site: "https://beauty-house-marijana.vercel.app",
