f1a10eff323dbf48281a98d487d8f21f25ac8ebe
Danijela Čolić <dgaric1@gmail.com>
2025-09-26 11:50:21 +0200

    resend6

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import react from "@astrojs/react";
-import vercel from "@astrojs/vercel/serverless"; // ili "/edge"
+import vercel from "@astrojs/vercel/edge"; // ⬅️ EDGE umjesto serverless
 
 export default defineConfig({
   site: "https://beauty-house-marijana.vercel.app",
   output: "server",
   adapter: vercel(),
-  integrations: [tailwind(), sitemap(), react()],
+  integrations: [tailwind(), sitemap()],
 });
